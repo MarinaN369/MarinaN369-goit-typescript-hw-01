e6ac4b220c7582ded2277a62402304ce4905fec7
MarinaN369 <143936204+MarinaN369@users.noreply.github.com>
2024-05-18 23:56:31 +0300

    update

---
+++ src/generics/5.ts
@@ -0,0 +1,16 @@
+export enum UserRole {
+    admin = "admin",
+    editor = "editor",
+    guest = "guest",
+  }
+  
+  
+  
+  type UserRoleDescriptions= Record<UserRole, string>;
+  
+  const RoleDescription: UserRoleDescriptions = {
+    [UserRole.admin]: "Admin User",
+    [UserRole.editor]: "Editor User",
+    [UserRole.guest]: "Guest User",
+  };
+  console.log(RoleDescription);
